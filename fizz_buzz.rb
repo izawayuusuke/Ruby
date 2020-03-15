8f89dff374b5ad9c4a9970f275a7f1084eb29db8
井澤優介 <izawayuusuke@MacBookPro.local>
2020-03-15 14:03:11 +0900

    FizzBuzzテスト自動化

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,19 +1,12 @@
-def fizz_buzz(number)
-    if number % 15 == 0
-    	puts "Fizz-Buzz"
-    elsif number % 3 == 0
-    	puts "Fizz"
-    elsif number % 5 == 0
-    	puts "Buzz"
-    else
-    	puts number.to_s
-    end
+def fizz_buzz(n)
+  if n % 15 == 0
+    'Fizz Buzz'
+  elsif n % 3 == 0
+  	'Fizz'
+  elsif n % 5 == 0
+  	'Buzz'
+  else
+  	n.to_s
+  end
 end
 
-puts "1以上の数字を入力してください"
-
-input = gets.to_i
-
-puts "結果"
-
-puts fizz_buzz(input)
